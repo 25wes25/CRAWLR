5b07daf61edf8ecc7a62284944ff93c011b6acd0
Mayra <msanchezk17@gmail.com>
2020-04-23 18:35:16 -0700

    Created Lyft&Uber buttons
    installed uber pods

---
+++ CRAWLR/DoneDrinkingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DoneDrinkingViewController.swift
+//  CRAWLR
+//
+//  Created by Mayra Sanchez on 4/21/20.
+//  Copyright © 2020 Wesley Swanson. All rights reserved.
+//
+
+import UIKit
+
+import Foundation
+
+class DoneDrinkingController: UIViewController{
+
+override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+}
+
+}
